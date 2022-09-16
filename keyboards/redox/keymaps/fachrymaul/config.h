c562087b456d696a06dbf686bb4d5fccf185eb0f
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2022-09-16 14:18:36 +0700

    change type layer bottom row, change nav layer

--- keyboards/redox/keymaps/fachrymaul/config.h
+++ keyboards/redox/keymaps/fachrymaul/config.h
@@ -34,4 +34,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_VAL_STEP 8
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_TERM 300
+#define TAPPING_TERM 500
